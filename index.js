51794ef4f78a2d704ef73344f12113ab0bf66568
dalimea818-ship-it <dalimea818@gmail.com>
2026-01-22 16:24:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,54 +9,44 @@ const io = new Server(server);
 app.use(express.json());
 app.use(express.static("public"));
 
-const onlineUsers = new Map(); // username -> socket.id
+const onlineUsers = new Map();
 
 io.on("connection", socket => {
-  console.log("Connected:", socket.id);
 
   socket.on("login", user => {
     socket.username = user.user;
     onlineUsers.set(user.user, socket.id);
-
-    io.emit("online-users", Array.from(onlineUsers.keys()));
+    io.emit("online-users", [...onlineUsers.keys()]);
   });
 
   socket.on("join-room", ({ me, other }) => {
-    const room = [me, other].sort().join("-");
-    socket.join(room);
+    socket.join([me, other].sort().join("-"));
   });
 
-  socket.on("send-msg", data => {
-    const room = [data.from, data.to].sort().join("-");
-    io.to(room).emit("receive-msg", data);
+  socket.on("send-msg", d => {
+    io.to([d.from, d.to].sort().join("-")).emit("receive-msg", d);
   });
 
-  // ---------- WEBRTC ----------
-  socket.on("call-user", ({ to, from, signal }) => {
-    const target = onlineUsers.get(to);
-    if (target) {
-      io.to(target).emit("incoming-call", { from, signal });
-    }
+  // --- WEBRTC ---
+  socket.on("call-user", d => {
+    const id = onlineUsers.get(d.to);
+    if (id) io.to(id).emit("incoming-call", d);
   });
 
-  socket.on("answer-call", ({ to, signal }) => {
-    const target = onlineUsers.get(to);
-    if (target) {
-      io.to(target).emit("call-answered", signal);
-    }
+  socket.on("answer-call", d => {
+    const id = onlineUsers.get(d.to);
+    if (id) io.to(id).emit("call-answered", d.signal);
   });
 
-  socket.on("ice-candidate", ({ to, candidate }) => {
-    const target = onlineUsers.get(to);
-    if (target) {
-      io.to(target).emit("ice-candidate", candidate);
-    }
+  socket.on("ice-candidate", d => {
+    const id = onlineUsers.get(d.to);
+    if (id) io.to(id).emit("ice-candidate", d.candidate);
   });
 
   socket.on("disconnect", () => {
     onlineUsers.delete(socket.username);
-    io.emit("online-users", Array.from(onlineUsers.keys()));
+    io.emit("online-users", [...onlineUsers.keys()]);
   });
 });
 
-server.listen(3000, () => console.log("Server running"));
+server.listen(3000, () => console.log("🚀 Server running"));
